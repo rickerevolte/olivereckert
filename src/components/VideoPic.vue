1ff9d5ce9223f8f56cf451a70968cadd4465543c
OliverE <mail@olivereckert.de>
2023-06-20 14:19:16 +0200

    modal behind side-menu-bug fixed

--- src/components/VideoPic.vue
+++ src/components/VideoPic.vue
@@ -1,58 +1,56 @@
 <template>
-    <div class="picContainer">
-        <div class="pic" @click="picClick(id)" @mouseover="picOver(id)">
-            <div class="vAlign">
-            <img :src="img"/>
-            <h5 class="white">{{title}}</h5>
-            </div>
-        </div>
-        <div class="title">
-
-        </div>
+  <div class="picContainer">
+    <div class="pic" @click="picClick(id)" @mouseover="picOver(id)">
+      <div class="vAlign">
+        <img :src="img" />
+        <h5 class="white">{{ title }}</h5>
+      </div>
     </div>
+    <div class="title"></div>
+  </div>
 </template>
 
 <script>
-import filmlist from '../../public/data/films.json';
+import filmlist from "../../public/data/films.json";
 export default {
-    props: ['img', 'id', 'title'],
-    emits: ['picClicked', 'picOver'],
-    methods: {
-        picClick() {
-            this.$emit("picClicked")
-        }  ,
-        picOver() {
-            this.$emit('picOver')
-        }
+  props: ["img", "id", "title"],
+  emits: ["picClicked", "picOver"],
+  methods: {
+    picClick() {
+      this.$emit("picClicked");
     },
-    created() {
-        this.films = filmlist;
-    }
-}
+    picOver() {
+      this.$emit("picOver");
+    },
+  },
+  created() {
+    this.films = filmlist;
+  },
+};
 </script>
 
 <style scoped>
 .picContainer {
-    display: flex;
-    margin-top: 3.5ch;
-    flex-direction: column;
+  display: flex;
+  margin-top: 3.5ch;
+  flex-direction: column;
 }
 .pic {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 200px;
-    height: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 290px;
+  aspect-ratio: 16/9;
 }
-img {    
-    width: 100%;
-    cursor: pointer;
+img {
+  width: 100%;
+  cursor: pointer;
 }
 .title {
-    width: 200px;
+  width: 200px;
 }
 .white {
-    color: white;
+  color: white;
 }
 </style>
